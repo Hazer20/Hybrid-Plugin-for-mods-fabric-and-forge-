b4e4bcd6244a19b4538988712519a4d84d6a212c
Hazer20 <kiiiillboy1213@gmail.com>
2026-03-07 18:10:54 +0200

    Add /maintenance on|off command to toggle maintenance mode

--- src/main/java/me/hazer/adminmaintenance/AdminMaintenance.java
+++ src/main/java/me/hazer/adminmaintenance/AdminMaintenance.java
@@ -1,5 +1,7 @@
 package me.hazer.adminmaintenance;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
@@ -10,6 +12,8 @@ public final class AdminMaintenance extends JavaPlugin implements Listener {
     private static final String MAINTENANCE_MESSAGE = "§cСервер временно закрыт.\n"
             + "§7Сейчас проводятся технические работы.\n"
             + "§eПопробуйте зайти позже.";
+
+    private boolean maintenanceEnabled = true;
 
     @Override
     public void onEnable() {
@@ -24,8 +28,40 @@ public final class AdminMaintenance extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerLogin(PlayerLoginEvent event) {
-        if (!event.getPlayer().isOp()) {
+        if (maintenanceEnabled && !event.getPlayer().isOp()) {
             event.disallow(PlayerLoginEvent.Result.KICK_OTHER, MAINTENANCE_MESSAGE);
         }
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!command.getName().equalsIgnoreCase("maintenance")) {
+            return false;
+        }
+
+        if (!sender.isOp()) {
+            sender.sendMessage("§cУ вас нет прав для этой команды.");
+            return true;
+        }
+
+        if (args.length != 1) {
+            sender.sendMessage("§eИспользование: /maintenance <on|off>");
+            return true;
+        }
+
+        if (args[0].equalsIgnoreCase("on")) {
+            maintenanceEnabled = true;
+            sender.sendMessage("§aРежим техработ включен.");
+            return true;
+        }
+
+        if (args[0].equalsIgnoreCase("off")) {
+            maintenanceEnabled = false;
+            sender.sendMessage("§aРежим техработ выключен.");
+            return true;
+        }
+
+        sender.sendMessage("§eИспользование: /maintenance <on|off>");
+        return true;
     }
 }
